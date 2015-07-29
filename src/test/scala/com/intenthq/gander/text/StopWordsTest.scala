dc07573abd13464e540ac5f5687a92db51852029
Artur Soler <artur@artursoler.com>
2015-07-29 15:10:25 +0200

    Fix tests

--- src/test/scala/com/intenthq/gander/text/StopWordsTest.scala
+++ src/test/scala/com/intenthq/gander/text/StopWordsTest.scala
@@ -5,13 +5,13 @@ import org.specs2.mutable.Specification
 class StopWordsTest extends Specification {
   "StopWords" >> {
     " should find how many stopwords are there" >> {
-      StopWords.getStopWordCount("blah blah blah").stopWordCount must_== 0
-      StopWords.getStopWordCount("although blah de blah").stopWordCount must_== 1
+      StopWords.stopWordCount("blah blah blah").stopWordCount must_== 0
+      StopWords.stopWordCount("although blah de blah").stopWordCount must_== 1
     }
 
     " should determine which words are stopwords" >> {
-      StopWords.getStopWordCount("although blah de blah").stopWords must_== List("although")
-      StopWords.getStopWordCount("blah de blah").stopWords must_== List.empty
+      StopWords.stopWordCount("although blah de blah").stopWords must_== List("although")
+      StopWords.stopWordCount("blah de blah").stopWords must_== List.empty
     }
   }
 }
